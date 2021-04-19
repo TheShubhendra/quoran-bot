6e46f19894106f935490f648721d4b1e111e7556
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2021-04-19 09:39:36 +0530

    Change welcome text and assign unverified role" "

--- bot.py
+++ bot.py
@@ -19,13 +19,26 @@ async def on_ready():
 async def on_member_join(member):
     print(member)
     guild = member.guild
-    
-    to_send = """Hey {0.mention}, welcome to Quorans! 
-Before moving forward please head over to the <#776135350287728690> channel and introduce yourself. Making an introduction is mandatory, to gain access to the server.
+    welcome_text = """Hey {}, welcome to {}! 
+
+Please head over to the <#776135350287728690> and introduce yourself. Making an introduction is mandatory, to gain access to the server.
 Read the <#776147848709275679> channel and abide by it, please.
-For main announcements, we have <#776331788062687242> channel. You can take up self roles in <#776791052924616745> channel.
-Enjoy your stay here, have fun slight_smile""".format(member)
-    await guild.system_channel.send(to_send)
+For main announcements, we have <#776331788062687242> channel. You can take up self roles in <#776791052924616745> channel (after verification).
+
+Hope you enjoy your stay here :slight_smile:""".format(member.mention, guild.name)
+    try:
+        role = guild.get_role(833512266934386728)
+        await member.add_roles(role, reason = "g")
+        await guild.system_channel.send(welcome_text)
+    except Exception as e:
+        print(e)
+
+async def get_quora(username):
+    user = User(username)
+  #  if user.firstName is not None and user.lastName is not None:
+        #embed = discord.Embed(title=)
+  #  else:
+    #    embed = discord.Embed
 
 @client.event
 async def on_message(message):
@@ -34,10 +47,7 @@ async def on_message(message):
 
     if message.content.lower().startswith('!quora'):
         username = message.content[7:]
-        try:
-            await message.channel.send(User(username))
-        except Exception as e:
-            await message.channel.send(e)
-    
-    
+        await message.channel.send(User(username))
+
+
 client.run(TOKEN)
